3bb28b3dbe5beaff640cff6143cf0311e2cb7f40
MurtazaSKH <hashmi.lfc@gmail.com>
2018-02-21 23:55:59 +0500

    21/2/18 - Final update

--- node-01/server.js
+++ node-01/server.js
@@ -6,28 +6,42 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 // var http = require('http');
 
-app.use(bodyParser.urlencoded({extended:true}))
-
 app.listen(3000, function() {
   console.log('listening on 3000')
   console.log(__dirname)
 });
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+var db;
+MongoClient.connect('mongodb://localhost:27017/up_db_first', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('up_db_first') // assign the database to db
+
+
+})
+
 app.get('/', (req, res) => {
   // res.send('Hello World')
-  res.sendFile(__dirname + '/index.html')
+  // res.sendFile(__dirname + '/index.html')
+  var cursor = db.collection('quotes').find().toArray(function(err,results){
+    console.log(results)
+  })
 
 })
+
+
+
+
+
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-// var db;
-MongoClient.connect('http://localhost:27017/', (err, client) => {
-  if (err) return console.log(err)
-  db = client.db('up_db_first') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
+    console.log('saved to database')
+    res.redirect('/')
   })
 })
