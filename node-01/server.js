ae230200f7b8c34af5cb9984e0d4148689b6e8d1
MSKH7 <34119910+MSKH7@users.noreply.github.com>
2018-02-22 15:22:48 +0500

    22/2/18 - Update to node-01 project before using gulp-nunjucks-render

--- node-01/server.js
+++ node-01/server.js
@@ -1,47 +1,53 @@
-console.log('May Node be with you');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+var nunjucksRender = require('gulp-nunjucks-render');
 // var http = require('http');
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-  console.log(__dirname)
-});
+// app.listen(3000, function() {
+//   console.log('listening on 3000')
+//   console.log(__dirname)
+// });
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-var db;
-MongoClient.connect('mongodb://localhost:27017/up_db_first', (err, client) => {
-  if (err) return console.log(err)
-  db = client.db('up_db_first') // assign the database to db
-
-
-})
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }))
+//
+// var db;
+// MongoClient.connect('mongodb://localhost:27017/up_db_first', (err, client) => {
+//   if (err) return console.log(err)
+//   db = client.db('up_db_first') // assign the database to db
+//
+//
+// })
 
 app.get('/', (req, res) => {
   // res.send('Hello World')
   // res.sendFile(__dirname + '/index.html')
-  var cursor = db.collection('quotes').find().toArray(function(err,results){
-    console.log(results)
-  })
+  // var cursor = db.collection('quotes').find().toArray(function(err,results){
+  //   console.log(results)
+  // })
 
 })
 
 
+// app.post('/quotes', (req, res) => {
+//   db.collection('quotes').save(req.body, (err, result) => {
+//     if (err) return console.log(err)
+//
+//     console.log('saved to database')
+//     res.redirect('/')
+//   })
+// })
 
-
-
-
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('node-01/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['node-01/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('node-01'))
+});
