ec0d7412d8254b564b3de91042b54a72bb971334
MSKH7 <34119910+MSKH7@users.noreply.github.com>
2018-02-27 23:26:25 +0500

    27/2/18 - SQL final

--- unityplugins-site/fetchdata/fetch.php
+++ unityplugins-site/fetchdata/fetch.php
@@ -95,8 +95,7 @@ if($func=="updatePluginModal")
 
 
                     echo '  </ul>
-                      <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-                      <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+                      
                     </div>
 
                   </div>';
