7ee60923cdbfe7a5d0af09be5754696f11d34f95
MSKH7 <34119910+MSKH7@users.noreply.github.com>
2018-02-27 08:55:25 +0500

    26/2/18 - Half way there with UP fetch data

--- unityplugins-site/fetchdata/fetch.js
+++ unityplugins-site/fetchdata/fetch.js
@@ -1,79 +1,62 @@
 var xmlhttp;
 
 ///////////////////////////////////////////////////// Ajax web service
-function GetXmlHttpObject()
-{
-if (window.XMLHttpRequest)
-  {
-  // code for IE7+, Firefox, Chrome, Opera, Safari
-  return new XMLHttpRequest();
+function GetXmlHttpObject() {
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    return new XMLHttpRequest();
   }
-if (window.ActiveXObject)
-  {
-  // code for IE6, IE5
-  return new ActiveXObject("Microsoft.XMLHTTP");
+  if (window.ActiveXObject) {
+    // code for IE6, IE5
+    return new ActiveXObject("Microsoft.XMLHTTP");
   }
-return null;
+  return null;
 }
 
 ///////////////////////////////////////////////////// load plugins list
-function login()
-{
- xmlhttp=GetXmlHttpObject();
- if (xmlhttp==null)
- {
- alert ("Browser does not support HTTP Request");
- return;
- }
- var user=document.getElementById("username").value;
- var pass=document.getElementById("password").value;
- var loggedin=document.getElementById("loggedin").checked;
+function getList() {
+  xmlhttp = GetXmlHttpObject();
+  if (xmlhttp == null) {
+    alert("Browser does not support HTTP Request");
+    return;
+  }
 
-
- if(loggedin==true)
- {
-   // global variable so the cookies can be saved
-   usercook=user;
-   passcook=pass;
- }
-
- // attempt to check for user/pass in database
- if(user != "" && pass !="")
- {
-   var url = "./includes/ajax.php";
-   url = url + "?func=login&user="+user+"&pass="+pass+"&loggedin="+loggedin;
-   url=url+"&sid="+Math.random();
-   xmlhttp.onreadystatechange=login_callback;
-   xmlhttp.open("GET",url,true);
-   xmlhttp.send(null);
-   //alert(url);
- }
- else
- {
-   notifContent = '<div class="alert alert-dark media fade in bd-0" style="background:#920000;" id="message-alert"><div class="media-body width-100p"><h4 class="alert-title f-14">Could not log you in!</h4><p class="f-12 alert-message pull-left">Please do not leave field(s) empty</p><p class="pull-right"></p></div></div>';
-       setTimeout(function() {
-           if (!$('#quickview-sidebar').hasClass('open') && !$('.page-content').hasClass('page-builder') && !$('.morphsearch').hasClass('open')) generateError(notifContent);
-       }, 100);
- }
+  // attempt to check for user/pass in database
+  var url = "./fetchdata/fetch.php";
+  url = url + "?func=displayAllPlugins";
+  url = url + "&sid=" + Math.random();
+  xmlhttp.onreadystatechange = getList_callback;
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send(null);
 }
 
-function login_callback()
-{
-if (xmlhttp.readyState==4)
-{
- var response= xmlhttp.responseText;
- if(response.length<14)
- {
-   window.location.href=response;
-   document.cookie = "username="+usercook+"; expires=Sun, 1 Jan 2017 12:00:00 UTC; path=/";
-   document.cookie = "password="+passcook+"; expires=Sun, 1 Jan 2017 12:00:00 UTC; path=/";
- }
- else
- {
-   notifContent = '<div class="alert alert-dark media fade in bd-0" style="background:#920000;" id="message-alert"><div class="media-body width-100p"><h4 class="alert-title f-14">Could not log you in!</h4><p class="f-12 alert-message pull-left">'+response+'</p><p class="pull-right"></p></div></div>';
-   setTimeout(function() {
-       if (!$('#quickview-sidebar').hasClass('open') && !$('.page-content').hasClass('page-builder') && !$('.morphsearch').hasClass('open')) generateError(notifContent);
-   }, 100);
- }
+function getList_callback() {
+  if (xmlhttp.readyState == 4) {
+    var response = xmlhttp.responseText;
+    document.getElementById("plugins_list").innerHTML = response
+  }
 }
+
+///////////////////////////////////////////////////// Update modal info and load
+function updateModal(pluginId) {
+  xmlhttp = GetXmlHttpObject();
+  if (xmlhttp == null) {
+    alert("Browser does not support HTTP Request");
+    return;
+  }
+
+  // attempt to check for user/pass in database
+  var url = "./fetchdata/fetch.php";
+  url = url + "?func=updatePluginModal&pluginId="+pluginId;
+  url = url + "&sid=" + Math.random();
+  xmlhttp.onreadystatechange = updateModal_callback;
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send(null);
+}
+
+function updateModal_callback() {
+  if (xmlhttp.readyState == 4) {
+    var response = xmlhttp.responseText;
+    document.getElementById("plugin").innerHTML = response;
+  }
 }
--- unityplugins-site/fetchdata/fetch.php
+++ unityplugins-site/fetchdata/fetch.php
@@ -10,26 +10,113 @@ if($func=="displayAllPlugins")
 	$query=("SELECT * FROM plugin_main");
 
    	$result = mysqli_query($db,$query);
-	if(mysqli_num_rows($result2) > 0)
+	if(mysqli_num_rows($result) > 0)
      {
-       echo '<div id="numberofrows" style="display:none;">'.mysqli_num_rows($result2).'</div>';
-       while($row2 = mysqli_fetch_array($result2,MYSQLI_BOTH))
-       {
-          $timestamp = strtotime($row2['LastUpdate']);
-           echo '<li id="device'.$row2['DeviceID'].'" class="" onclick="clearonedevice('.$row2['DeviceID'].')" ';
-           if ($row2['OSType']=='iOS')
-           {
-             echo 'style="background-color:#424F63;"';
-           }
-           echo '><span class="span-check" >
-     <label for="task-2" class="">'.$row2['name'].' - v'.$row2['OSVersion'].'</label>
-     </span>
 
-     <div class="todo-date">
-       <div class="completed-date"> '.$row2['OSType'].'</div>
-       <div class="due-date">'.date('g:i a', $timestamp).' - '.date('l, F d, Y', $timestamp).'</div>
-     </div>
-     </li>';
+
+
+
+       while($row = mysqli_fetch_array($result,MYSQLI_BOTH))
+       {
+         echo '<div class="col col-lg-3 '.$row['category'].'" >
+
+           <div class="uk-inline uk-slider-items uk-transition-toggle ">
+             <img src="'.$row['default_thumb'].'" alt="">
+             <div class="uk-overlay uk-overlay-primary uk-remove-padding uk-position-bottom uk-transition-fade" style="min-height:50%;">
+               <a href="#plugin" onclick="updateModal('.$row['plugin_id'].')" uk-toggle>
+                 <div class=" uk-overlay-primary uk-transition-fade  uk-width-1-1 uk-position-top uk-text-center  ">
+                   Quick View
+                 </div>
+               </a>
+               <p class="uk-padding-remove">'.$row['title'].'</p>
+             </div>
+           </div>
+         </div>
+         ';
+        }
+       }
+
+}
+
+////////////////////////////////////////// get data for selected plugin //////////////////////////////////
+
+if($func=="updatePluginModal")
+{
+  $pluginId = $_REQUEST['pluginId'];
+
+	$query=("SELECT * FROM plugin_main WHERE plugin_id='$pluginId'");
+
+   	$result = mysqli_query($db,$query);
+	if(mysqli_num_rows($result) > 0)
+     {
+
+
+
+
+       while($row = mysqli_fetch_array($result,MYSQLI_BOTH))
+       {
+         echo '<div class="uk-modal-dialog uk-modal-body">
+           <button class="uk-modal-close-default" type="button" uk-close></button>
+           <div class="uk-grid-collapse uk-child-width-1-1@s uk-flex-middle" uk-grid>
+             <h1 class="uk-text-center .uk-text-small" style="font-size: 1.5rem !important;">WRP IOS Video Player</h1>
+             <div class="" name="col1" style="">
+               <div class="uk-padding-small" uk-slideshow uk-grid>
+                 <div class="uk-width-auto@m">
+                   <ul class="uk-thumbnav uk-thumbnav-vertical" uk-margin>
+                     <li uk-slideshow-item="0"><a href="#"><img src="./images/plu1.jpg" width="60" alt=""></a></li>
+                     <li uk-slideshow-item="1"><a href="#"><img src="./images/plu2.jpg" width="60" alt=""></a></li>
+                     <li uk-slideshow-item="2"><a href="#"><img src="./images/plu3.jpg" width="60" alt=""></a></li>
+                     <li uk-slideshow-item="3"><a href="#"><img src="./images/youtube.jpg" width="60" alt=""></a></li>
+                   </ul>
+                 </div>
+                 <div class="uk-position-relative uk-visible-toggle uk-dark uk-width-expand@m">
+                   <ul class="uk-slideshow-items">
+                     <li>
+                       <img src="./images/plu1.jpg" alt="" uk-cover>
+                     </li>
+                     <li>
+                       <img src="./images/plu2.jpg" alt="" uk-cover>
+                     </li>
+                     <li>
+                       <img src="./images/plu3.jpg" alt="" uk-cover>
+                     </li>
+                     <li>
+                       <iframe width="440" height="315" src="https://www.youtube.com/embed/TUUUh-51t1k?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                     </li>
+                   </ul>
+                   <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+                   <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+                 </div>
+
+               </div>
+
+               <!-- <div class="uk-padding-small"> -->
+               <table class="uk-table uk-table-hover uk-table-divider uk-dark uk-margin-remove-bottom">
+                 <tbody>
+                   <tr>
+                     <td style="color:#333333">Price: $</td>
+                     <td style="color:#333333">Supported Versions: </td>
+                   </tr>
+                   <tr>
+                     <td><a href="">Download</a></td>
+                     <td></td>
+                   </tr>
+                 </tbody>
+               </table>
+               <!-- </div> -->
+             </div>
+             <div class="uk-padding-small">
+
+               <p>
+                 <div >
+                   '.$row['Description'].'
+                 </div>
+               </p>
+             </div>
+           </div>
+         </div>';
+
+
         }
        }
 
