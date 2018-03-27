ca9fcbc7c101e173e14b9abd55b0bb7b11c08eec
MurtazaSKH <hashmi.lfc@gmail.com>
2018-03-27 16:34:24 +0500

    Classes in Main.js server
    27/3/18 - Deleted after this update

--- meteor-03/server/main.js
+++ meteor-03/server/main.js
@@ -2,13 +2,48 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() =>{
-//   Players.insert(
-// {
-//   name: 'Milner',
-//   number:7
-// });
-  // console.log(Players.find().fetch());
-  let numbers = [9,99,4,56];
-  let newNumbers= numbers.map((number) => number+1);
-  console.log(newNumbers);
+  class Person {
+    constructor (name='anonymous',age=0){
+      this.name=name;
+      this.age=age;
+    }
+    getGreeting() {
+      // getAge();
+      return 'Hi I am '+this.name+'';
+    }
+    getPersonDescription(){
+      return ''+this.name+' is '+this.age+' years old';
+    }
+  }
+
+  class Employee extends Person {
+    constructor (name, age,title) {
+      super(name,age);
+      this.title=title;
+    }
+    getGreeting () {
+      if(this.title) {
+        return ''+this.name+' is '+this.age+' years old and this is my job title '+this.title+'';
+      }
+      else {
+        return super.getGreeting();
+      }
+    }
+    hasJob (){
+      return !!this.title;
+    }
+  }
+
+  class Programmer extends Person {
+    constructor (name,age,language ="assembly"){
+      super(name,age);
+      this.language=language;
+    }
+    getGreeting () {
+      return 'Hi i am '+this.name+' and i am a '+this.language+' Programmer. ';
+    }
+  }
+
+  let person = new Programmer('Emma',32,'C#');
+  console.log(person.getGreeting());
 });
